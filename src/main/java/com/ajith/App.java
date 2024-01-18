6e7b30cd6e0ff0b71b9b316c4fc5fe86e9006fa4
Ajith krkd <ajith2255iti@gmail.com>
2024-01-18 20:18:08 +0530

    created a simple java application

---
+++ src/main/java/com/ajith/App.java
@@ -0,0 +1,13 @@
+package com.ajith;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
