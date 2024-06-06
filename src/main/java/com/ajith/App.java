c1de5a5e34295d81bd57080d7b6806157f8e77e6
Ajith krkd <ajith2255iti@gmail.com>
2024-06-06 22:01:47 +0530

    nothing

--- src/main/java/com/ajith/App.java
+++ src/main/java/com/ajith/App.java
@@ -1,18 +1,9 @@
 package com.ajith;
 
-import com.ajith.strings.StringExamples;
-
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        Calculator calculator = new Calculator();
-        int a = calculator.sumNumber ( 1,2 );
-        System.out.println (a + "        anser" );
+
     }
 }
